b18a40c0b7e196b7f5b3dd35ca6b0be3c0732053
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-10-02 23:15:04 -0500

    layout started with css styling

---
+++ Assets/js/weather.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    //get current date from Moment.js
+    var momentDateString = moment().toString();
+    var momentDate = moment().toDate();
+
+    console.log(momentDateString);
+    console.log(momentDate);
+
+    /*
+    //Create dynamic HTML elements
+    var row1DivEl = $("<div>");
+    row1DivEl.addClass("row");
+    var
+
+    var row2DivEl = $("<div>");
+    row2DivEl.addClass("row");
+    $(".container").append(rowDivEl, row2DivEl);
+    
+    var
+*/
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather_Dashboard
+
+
+## Review
+
+* The URL of the deployed application - 
+
+* The URL of the GitHub repository - https://github.com/meenaambalam/Weather_Dashboard
